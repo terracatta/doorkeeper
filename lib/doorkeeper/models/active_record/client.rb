719620a392df4465fa437c6b982179a65a5d4e50
Felipe Elias Philipp <felipeelias@gmail.com>
2012-11-10 23:36:25 +0100

    Doorkeeper.client instead

--- lib/doorkeeper/models/active_record/client.rb
+++ lib/doorkeeper/models/active_record/client.rb
@@ -12,6 +12,9 @@ module Doorkeeper
           before_validation :generate_uid, :generate_secret, :on => :create
 
           attr_accessible :name, :redirect_uri
+
+          has_many :authorized_tokens, :class_name => "Doorkeeper::AccessToken", :conditions => { :revoked_at => nil }, :foreign_key => 'application_id'
+          has_many :authorized_applications, :through => :authorized_tokens, :source => :application
         end
 
         module ClassMethods
@@ -25,6 +28,17 @@ module Doorkeeper
 
           def by_uid(uid)
             where(:uid => uid).first
+          end
+
+          def column_names_with_table
+            self.column_names.map { |c| "oauth_applications.#{c}" }
+          end
+
+          # TODO: Authorized tokens should not be mixed in into client's class
+          def authorized_for(resource_owner)
+            joins(:authorized_applications).
+              where(:oauth_access_tokens => { :resource_owner_id => resource_owner.id, :revoked_at => nil }).
+              group(column_names_with_table.join(','))
           end
         end
 
@@ -43,7 +57,7 @@ module Doorkeeper
         extend ActiveSupport::Concern
 
         included do
-          belongs_to :application, :class_name => Doorkeeper.client
+          belongs_to :application, :class_name => Doorkeeper.client, :foreign_key => 'application_id'
         end
       end
     end
--- spec/models/doorkeeper/application_spec.rb
+++ spec/models/doorkeeper/application_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper_integration'
 
 module Doorkeeper
-  describe Application do
+  describe 'Application (aka Client)' do
     let(:new_application) { FactoryGirl.build(:application) }
 
     it 'is invalid without a name' do
@@ -78,31 +78,31 @@ module Doorkeeper
       let(:resource_owner) { double(:resource_owner, :id => 10) }
 
       it "is empty if the application is not authorized for anyone" do
-        Application.authorized_for(resource_owner).should be_empty
+        Doorkeeper.client.authorized_for(resource_owner).should be_empty
       end
 
       it "returns only application for a specific resource owner" do
         FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id + 1)
         token = FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id)
-        Application.authorized_for(resource_owner).should == [token.application]
+        Doorkeeper.client.authorized_for(resource_owner).should == [token.application]
       end
 
       it "excludes revoked tokens" do
         FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id, :revoked_at => 2.days.ago)
-        Application.authorized_for(resource_owner).should be_empty
+        Doorkeeper.client.authorized_for(resource_owner).should be_empty
       end
 
       it "returns all applications that have been authorized" do
         token1 = FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id)
         token2 = FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id)
-        Application.authorized_for(resource_owner).should == [token1.application, token2.application]
+        Doorkeeper.client.authorized_for(resource_owner).should == [token1.application, token2.application]
       end
 
       it "returns only one application even if it has been authorized twice" do
         application = FactoryGirl.create(:application)
         FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id, :application => application)
         FactoryGirl.create(:access_token, :resource_owner_id => resource_owner.id, :application => application)
-        Application.authorized_for(resource_owner).should == [application]
+        Doorkeeper.client.authorized_for(resource_owner).should == [application]
       end
 
       it "should fail to mass assign a new application" do
@@ -110,14 +110,14 @@ module Doorkeeper
                         :redirect_uri => 'http://somewhere.com/something',
                         :uid => 123,
                         :secret => 'something' }
-        Application.create(mass_assign).uid.should_not == 123
+        Doorkeeper.client.create(mass_assign).uid.should_not == 123
       end
     end
 
     describe :authenticate do
       it 'finds the application via uid/secret' do
         app = FactoryGirl.create :application
-        authenticated = Application.authenticate(app.uid, app.secret)
+        authenticated = Doorkeeper.client.authenticate(app.uid, app.secret)
         authenticated.should == app
       end
     end
